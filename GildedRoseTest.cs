06e41d1f6910aa5ecc6431460409bfaf4c0e2220
saulius <saulius@cebanauskai.lt>
2020-03-06 07:50:47 +0200

    Added Conjured product to unit test test cases. Fixed other products corner cases

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -9,13 +9,13 @@ namespace csharpcore
     {
         [Theory]
         //0 sellin corner cases
-        [InlineData(1, 1, 3)]
-        [InlineData(2, 0, 2)]
-        [InlineData(3, -1, 0)]
+        [InlineData(1, 1, 4)]
+        [InlineData(2, 0, 3)]
+        [InlineData(3, -1, 1)]
         [InlineData(4, -2, 0)]
         public void CommonProductShouldDecreaseCorrectly(int daysToDecrease, int expectedSellIn, int expectedQuality)
         {
-            GeneralSingleProductTest(new Item { Name = "product", SellIn = 2, Quality = 4 }, daysToDecrease, expectedSellIn, expectedQuality);
+            GeneralSingleProductTest(new Item { Name = "product", SellIn = 2, Quality = 5 }, daysToDecrease, expectedSellIn, expectedQuality);
         }
 
         [Theory]
@@ -34,21 +34,21 @@ namespace csharpcore
 
         [Theory]
         //10 sellin corner cases
-        [InlineData(1, 11, 24)]
-        [InlineData(2, 10, 25)]
-        [InlineData(3, 9, 27)]
+        [InlineData(1, 11, 25)]
+        [InlineData(2, 10, 26)]
+        [InlineData(3, 9, 28)]
         //5 sellin corner cases
-        [InlineData(6, 6, 33)]
-        [InlineData(7, 5, 35)]
-        [InlineData(8, 4, 38)]
-        //0 sellin corner cases
-        [InlineData(11, 1, 47)]
+        [InlineData(6, 6, 34)]
+        [InlineData(7, 5, 36)]
+        [InlineData(8, 4, 39)]
+        ////0 sellin corner cases
+        [InlineData(11, 1, 48)]
         [InlineData(12, 0, 50)]
         [InlineData(13, -1, 0)]
         [InlineData(14, -2, 0)]
         public void BackstagepassesShouldDecreaseCorrectly(int daysToDecrease, int expectedSellIn, int expectedQuality)
         {
-            GeneralSingleProductTest(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 12, Quality = 23 }, daysToDecrease, expectedSellIn, expectedQuality);
+            GeneralSingleProductTest(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 12, Quality = 24 }, daysToDecrease, expectedSellIn, expectedQuality);
         }
 
         [Theory]
@@ -63,6 +63,17 @@ namespace csharpcore
         public void NegativeSellInSulfurasShouldNeverChange(int daysToDecrease, int expectedSellIn, int expectedQuality)
         {
             GeneralSingleProductTest(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -2, Quality = 80 }, daysToDecrease, expectedSellIn, expectedQuality);
+        }
+
+        [Theory]
+        //0 sellin corner cases
+        [InlineData(1, 1, 7, Skip = "not implemented")]
+        [InlineData(2, 0, 5, Skip = "not implemented")]
+        [InlineData(3, -1, 1, Skip = "not implemented")]
+        [InlineData(4, -2, 0, Skip = "not implemented")]
+        public void ConjuredShouldDecreaseCorrectly(int daysToDecrease, int expectedSellIn, int expectedQuality)
+        {
+            GeneralSingleProductTest(new Item { Name = "Conjured Mana Cake", SellIn = 2, Quality = 9 }, daysToDecrease, expectedSellIn, expectedQuality);
         }
 
         private void GeneralSingleProductTest(Item itemToTest, int daysToDecrease, int expectedSellIn, int expectedQuality)
