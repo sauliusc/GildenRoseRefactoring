b69536e0b9612142052e338d13e10d23d4e96108
saulius <saulius@cebanauskai.lt>
2020-03-06 07:32:39 +0200

    Added unit tests for corner quality and sellin update cases

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -1,17 +1,85 @@
 ﻿using Xunit;
 using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 
 namespace csharpcore
 {
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
+        [Theory]
+        //0 sellin corner cases
+        [InlineData(1, 1, 3)]
+        [InlineData(2, 0, 2)]
+        [InlineData(3, -1, 0)]
+        [InlineData(4, -2, 0)]
+        public void CommonProductShouldDecreaseCorrectly(int daysToDecrease, int expectedSellIn, int expectedQuality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            GeneralSingleProductTest(new Item { Name = "product", SellIn = 2, Quality = 4 }, daysToDecrease, expectedSellIn, expectedQuality);
+        }
+
+        [Theory]
+        //0 sellin corner cases
+        [InlineData(1, 1, 1)]
+        [InlineData(2, 0, 2)]
+        [InlineData(3, -1, 4)]
+        //50 quality corner cases
+        [InlineData(25, -23, 48)]
+        [InlineData(26, -24, 50)]
+        [InlineData(27, -25, 50)]
+        public void AgedBrieShouldDecreaseCorrectly(int daysToDecrease, int expectedSellIn, int expectedQuality)
+        {
+            GeneralSingleProductTest(new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 }, daysToDecrease, expectedSellIn, expectedQuality);
+        }
+
+        [Theory]
+        //10 sellin corner cases
+        [InlineData(1, 11, 24)]
+        [InlineData(2, 10, 25)]
+        [InlineData(3, 9, 27)]
+        //5 sellin corner cases
+        [InlineData(6, 6, 33)]
+        [InlineData(7, 5, 35)]
+        [InlineData(8, 4, 38)]
+        //0 sellin corner cases
+        [InlineData(11, 1, 47)]
+        [InlineData(12, 0, 50)]
+        [InlineData(13, -1, 0)]
+        [InlineData(14, -2, 0)]
+        public void BackstagepassesShouldDecreaseCorrectly(int daysToDecrease, int expectedSellIn, int expectedQuality)
+        {
+            GeneralSingleProductTest(new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 12, Quality = 23 }, daysToDecrease, expectedSellIn, expectedQuality);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 80)]
+        public void SulfurasShouldNeverChange(int daysToDecrease, int expectedSellIn, int expectedQuality)
+        {
+            GeneralSingleProductTest(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 2, Quality = 80 }, daysToDecrease, expectedSellIn, expectedQuality);
+        }
+
+        [Theory]
+        [InlineData(1, -2, 80)]
+        public void NegativeSellInSulfurasShouldNeverChange(int daysToDecrease, int expectedSellIn, int expectedQuality)
+        {
+            GeneralSingleProductTest(new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -2, Quality = 80 }, daysToDecrease, expectedSellIn, expectedQuality);
+        }
+
+        private void GeneralSingleProductTest(Item itemToTest, int daysToDecrease, int expectedSellIn, int expectedQuality)
+        {
+            GildedRose gildedRose = new GildedRose(new[] { itemToTest });
+            RepeatUpdateQuality(gildedRose, daysToDecrease);
+            itemToTest.SellIn.Should().Be(expectedSellIn);
+            itemToTest.Quality.Should().Be(expectedQuality);
+        }
+
+
+        private void RepeatUpdateQuality(GildedRose gildenRose, int repeatCount)
+        {
+            for (int i = 0; i < repeatCount; i++)
+            {
+                gildenRose.UpdateQuality();
+            }
         }
     }
 }
