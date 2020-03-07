d6714ff96d7fa7be876e57102196fb6d4f7832d6
saulius <saulius@cebanauskai.lt>
2020-03-07 08:49:33 +0200

    Updated UpdateQuality to use switch statement

--- GildedRose.cs
+++ GildedRose.cs
@@ -20,48 +20,48 @@ namespace csharpcore
 
                 sellItem.SellIn--;
 
-                if (sellItem.Name.Equals("Aged Brie"))
+                switch (sellItem.Name)
                 {
-                    if (CanIncreaseQuality(sellItem))
-                    {
-                        sellItem.Quality++;
-                        if (sellItem.SellIn < 0 && CanIncreaseQuality(sellItem))
+                    case ("Aged Brie"):
+                        if (CanIncreaseQuality(sellItem))
                         {
                             sellItem.Quality++;
+                            if (sellItem.SellIn < 0 && CanIncreaseQuality(sellItem))
+                            {
+                                sellItem.Quality++;
+                            }
                         }
-                    }
-                }
-                else if (sellItem.Name.Equals("Backstage passes to a TAFKAL80ETC concert"))
-                {
-                    if (sellItem.SellIn < 0)
-                    {
-                        sellItem.Quality = 0;
-                    }
-                    else if (CanIncreaseQuality(sellItem))
-                    {
-                        sellItem.Quality++;
+                        break;
+                    case ("Backstage passes to a TAFKAL80ETC concert"):
+                        if (sellItem.SellIn < 0)
+                        {
+                            sellItem.Quality = 0;
+                        }
+                        else if (CanIncreaseQuality(sellItem))
+                        {
+                            sellItem.Quality++;
 
-                        if (sellItem.SellIn < 10 && CanIncreaseQuality(sellItem))
-                        {
-                            sellItem.Quality++;
-                        }
+                            if (sellItem.SellIn < 10 && CanIncreaseQuality(sellItem))
+                            {
+                                sellItem.Quality++;
+                            }
 
-                        if (sellItem.SellIn < 5 && CanIncreaseQuality(sellItem))
-                        {
-                            sellItem.Quality++;
+                            if (sellItem.SellIn < 5 && CanIncreaseQuality(sellItem))
+                            {
+                                sellItem.Quality++;
+                            }
                         }
-                    }
-                }
-                else
-                {
-                    if (sellItem.Quality > 0)
-                    {
-                        sellItem.Quality--;
-                    }
-                    if (sellItem.SellIn < 0 && sellItem.Quality > 0)
-                    {
-                        sellItem.Quality--;
-                    }
+                        break;
+                    default:
+                        if (sellItem.Quality > 0)
+                        {
+                            sellItem.Quality--;
+                        }
+                        if (sellItem.SellIn < 0 && sellItem.Quality > 0)
+                        {
+                            sellItem.Quality--;
+                        }
+                        break;
                 }
             }
         }
